96c6cd8a0d027190c69530658b801999ffbb5ad6
Pavel Bu <puchik006@mail.ru>
2023-09-26 16:14:22 +0300

    Bullet

---
+++ Assets/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float destroyDelay = 0.5f; // Adjust the delay before the bullet is destroyed.
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the bullet collides with something other than the player.
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            // Disable the bullet's renderer and collider.
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<Collider2D>().enabled = false;
+
+            // Destroy the bullet after a delay.
+            Destroy(gameObject, destroyDelay);
+        }
+    }
+}
---
+++ Assets/PlayerShooting.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject bulletPrefab; // Assign your bullet prefab in the Unity Inspector.
+    public Transform firePoint; // Point from which bullets will be spawned.
+    public float bulletSpeed = 10f; // Speed of the bullets.
+    public float fireRate = 0.5f; // Fire rate in seconds.
+    private float nextFireTime = 0f; // Time at which the next shot can be fired.
+
+    void Update()
+    {
+        // Check if it's time to fire again.
+        if (Time.time >= nextFireTime)
+        {
+            // Check for fire input (e.g., left mouse button or any key).
+            if (Input.GetButtonDown("Fire1")) // Change "Fire1" to your desired input.
+            {
+                Shoot();
+                nextFireTime = Time.time + 1f / fireRate; // Calculate the next allowed fire time.
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        // Instantiate a bullet at the fire point's position and rotation.
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        // Get the bullet's Rigidbody2D component.
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+
+        // Set the bullet's velocity to make it move in the forward direction.
+        rb.velocity = transform.right * bulletSpeed;
+    }
+}
